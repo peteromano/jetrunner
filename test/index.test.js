dbccd6f6e7bea2494b69928f7a471b96298cc2d1
Pete Romano <pete.romano@yahoo.com>
2012-09-22 21:17:46 -0400

    Refactored scripts

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 var vows = require('vows'),
 	assert = require('assert'),
-    jetrunner = require('../lib/jetrunner');
+    jetrunner = require('../lib');
 
 vows.describe('JetRunner').addBatch({
 
