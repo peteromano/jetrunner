c525c123c102a124fc6d0d23c1a910f597b9b799
Pete Romano <pete.romano@yahoo.com>
2012-09-28 20:08:51 -0400

    Make app.js configurable

--- server/app.js
+++ server/app.js
@@ -1,34 +1,37 @@
+module.exports = function(options) {
+    /**
+     * Module dependencies.
+     */
 
-/**
- * Module dependencies.
- */
+    var express = require('express')
+      , routes = require('./routes')
+      , http = require('http')
+      , path = require('path');
 
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path');
+    var app = express();
 
-var app = express();
+    options = options || {};
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  //app.use(express.static(path.join(__dirname, 'public')));
-  app.use(express.static(process.cwd()));
-});
+    app.configure(function(){
+      app.set('port', options.port || process.env.PORT || 3000);
+      app.set('views', __dirname + '/views');
+      app.set('view engine', 'jade');
+      app.use(express.favicon());
+      app.use(express.logger('dev'));
+      app.use(express.bodyParser());
+      app.use(express.methodOverride());
+      app.use(app.router);
+      app.use(express.static(options.base && path.resolve(options.base) || process.cwd()));
+    });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+    app.configure('development', function(){
+      app.use(express.errorHandler());
+    });
 
-app.get('/', routes.index);
+    app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+    http.createServer(app).listen(app.get('port'), function(){
+      console.log("Express server listening on port " + app.get('port'));
+    });
+
+};
