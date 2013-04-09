5b0dd2049e04d4ec20f7faf87811d925d1c76bf5
Pete Romano <pete.romano@yahoo.com>
2013-04-09 14:07:50 -0400

    Small refactor

--- test/index.js
+++ test/index.js
@@ -1,3 +1,12 @@
+var jetrunner = require('../lib'),
+    assert = require('assert');
+
 describe('jetrunner', function() {
-    require('./version');
+
+    describe('#VERSION', function() {
+        it('should be at 1.x', function() {
+            assert.ok(/1.\d/.test(jetrunner.VERSION));
+        })
+    });
+
 });
--- test/version.js
+++
@@ -1,8 +0,0 @@
-var jetrunner = require('../lib'),
-    assert = require('assert');
-
-describe('#version()', function() {
-    it('should be at 1.x', function() {
-        assert.ok(/1.\d/.test(jetrunner.version()));
-    })
-});
